3be679fc4a4eb7f993ebf5ff3ba7cf16172f5ea4
Ari Amri <ariamri89@gmail.com>
2023-03-31 10:11:13 +0700

    update fix query dan realisasi beserta padding

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,19 +20,13 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-        $data['prog_sampah'] = $this->db->query("SELECT CAST((SUM(a.jumlah) / SUM(b.jumlah)) * 100 AS UNSIGNED) AS percentage
-                                   FROM progress_kegiatan a, target_kegiatan b WHERE b.idKhidmat = 1")->row();
-        $data['prog_administrasi'] = $this->db->query("SELECT CAST((SUM(a.jumlah) / SUM(b.jumlah)) * 100 AS UNSIGNED) AS percentage
-                                   FROM progress_kegiatan a, target_kegiatan b WHERE b.idKhidmat = 2")->row();
-        $data['prog_pembangunan'] = $this->db->query("SELECT CAST((SUM(a.jumlah) / SUM(b.jumlah)) * 100 AS UNSIGNED) AS percentage
-                                   FROM progress_kegiatan a, target_kegiatan b WHERE b.idKhidmat = 3")->row();
-        $data['prog_drainase'] = $this->db->query("SELECT CAST((SUM(a.jumlah) / SUM(b.jumlah)) * 100 AS UNSIGNED) AS percentage
-                                   FROM progress_kegiatan a, target_kegiatan b WHERE b.idKhidmat = 4")->row();
-        $data['prog_sosial'] = $this->db->query("SELECT CAST((SUM(a.jumlah) / SUM(b.jumlah)) * 100 AS UNSIGNED) AS percentage
-                                   FROM progress_kegiatan a, target_kegiatan b WHERE b.idKhidmat = 5")->row();
-        $data['prog_pelayanan'] = $this->db->query("SELECT CAST((SUM(a.jumlah) / SUM(b.jumlah)) * 100 AS UNSIGNED) AS percentage
-                                   FROM progress_kegiatan a, target_kegiatan b WHERE b.idKhidmat = 6")->row();
-		var_dump($data);
+        $data['prog_sampah'] = $this->db->query("SELECT (SUM(progress_kegiatan.jumlah) / SUM(target_kegiatan.jumlah)) * 100 AS percentage FROM target_kegiatan LEFT JOIN progress_kegiatan ON target_kegiatan.idTarget = progress_kegiatan.idTarget WHERE target_kegiatan.idKhidmat = 1;")->row();
+        $data['prog_administrasi'] = $this->db->query("SELECT (SUM(progress_kegiatan.jumlah) / SUM(target_kegiatan.jumlah)) * 100 AS percentage FROM target_kegiatan LEFT JOIN progress_kegiatan ON target_kegiatan.idTarget = progress_kegiatan.idTarget WHERE target_kegiatan.idKhidmat = 2")->row();
+        $data['prog_pembangunan'] = $this->db->query("SELECT (SUM(progress_kegiatan.jumlah) / SUM(target_kegiatan.jumlah)) * 100 AS percentage FROM target_kegiatan LEFT JOIN progress_kegiatan ON target_kegiatan.idTarget = progress_kegiatan.idTarget WHERE target_kegiatan.idKhidmat = 3")->row();
+        $data['prog_drainase'] = $this->db->query("SELECT (SUM(progress_kegiatan.jumlah) / SUM(target_kegiatan.jumlah)) * 100 AS percentage FROM target_kegiatan LEFT JOIN progress_kegiatan ON target_kegiatan.idTarget = progress_kegiatan.idTarget WHERE target_kegiatan.idKhidmat = 4")->row();
+        $data['prog_sosial'] = $this->db->query("SELECT (SUM(progress_kegiatan.jumlah) / SUM(target_kegiatan.jumlah)) * 100 AS percentage FROM target_kegiatan LEFT JOIN progress_kegiatan ON target_kegiatan.idTarget = progress_kegiatan.idTarget WHERE target_kegiatan.idKhidmat = 5")->row();
+        $data['prog_pelayanan'] = $this->db->query("SELECT (SUM(progress_kegiatan.jumlah) / SUM(target_kegiatan.jumlah)) * 100 AS percentage FROM target_kegiatan LEFT JOIN progress_kegiatan ON target_kegiatan.idTarget = progress_kegiatan.idTarget WHERE target_kegiatan.idKhidmat = 6")->row();
+		$data['realisasi'] = $this->db->query("SELECT opd.nama, SUM(target_kegiatan.budget) AS total_dana, COALESCE(SUM(progress.dana), 0) AS terealisasi, opd.logo, ROUND((COALESCE(SUM(progress.dana), 0) / SUM(target_kegiatan.budget) * 100), 2) AS percentage FROM opd INNER JOIN target_kegiatan ON opd.idOpd = target_kegiatan.idOpd LEFT JOIN ( SELECT idTarget, SUM(dana) AS dana FROM progress_kegiatan GROUP BY idTarget ) AS progress ON target_kegiatan.idTarget = progress.idTarget GROUP BY opd.nama, opd.logo;")->result();
 		$this->load->view('template/header');
 		$this->load->view('template/menu');
 		$this->load->view('home/index', $data);
